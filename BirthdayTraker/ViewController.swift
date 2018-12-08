663a9c432f79c7db8156dfa420b704cd94128ed1
Sergey Prokopyev <studnetserg@gmail.com>
2018-12-08 12:31:05 +0500

    First Commit
    First project

---
+++ BirthdayTraker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirthdayTraker
+//
+//  Created by Сергей Прокопьев on 08/12/2018.
+//  Copyright © 2018 someThing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
